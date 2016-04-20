1d69cd5d2438e412e2a9ca31bef5ac750053aed0
Jesse Millikan <jesse.a.millikan@gmail.com>
2016-04-19 21:38:52 -0400

    Failing spec added (app not configured for DB)

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,25 +3,23 @@
 {-# LANGUAGE QuasiQuotes #-}
 
 module Lib
-    ( runApp
+    ( runApp, app
     ) where
 
-import Control.Monad (forM_)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Logger
-import Control.Arrow
 import Data.Int (Int64)
 import Data.Aeson hiding (json)
-import Data.Text (Text)
+import Data.Text (Text, pack)
 import qualified Data.HashMap.Strict as HM (insert)
-import System.Environment
 
-import Database.Persist (update, (=.))
-import qualified Database.Persist as P
+-- These four identifiers are the subject of collisions *and* confusion...
+import qualified Database.Persist as P (get, update, insert) 
+import qualified Database.Esqueleto as E (select)
+import Database.Persist ((=.))
+import Database.Esqueleto hiding (update, get, Value, (=.), select)
 import qualified Database.Persist.Sqlite as Sqlite
-import Network.HTTP.Types.Status
 import Web.Spock.Safe
-import Database.Esqueleto hiding (update, get, Value, (=.))
 
 import Db
 
@@ -35,72 +33,76 @@ nest :: Value -> Text -> Value -> Value
 nest (Object outerMap) name inner = Object $ HM.insert name inner outerMap
 nest _ _ _ = error "Can only nest inside an object"
 
-runApp :: IO ()
-runApp = do
+-- P.get returns State, E.select returns Entity State.
+-- Trying not to intermix them for clarity
 
-  Sqlite.runSqlite "main.db" $ Sqlite.runMigration migrateAll
+runApp :: String -> Int -> IO ()
+runApp dbFilename portNum = do
+  Sqlite.runSqlite (pack dbFilename) $ Sqlite.runMigration migrateAll
 
   -- Go back over this in... "The future"
-  runNoLoggingT $ Sqlite.withSqlitePool "main.db" 10 $ \pool -> do
-    let withDb f = liftIO $ runSqlPersistMPool f pool
+  runNoLoggingT $ Sqlite.withSqlitePool (pack dbFilename) 10 $ \pool ->
+    NoLoggingT $ runSpock portNum $ app pool
+  
+app pool = spockT id $ do
+  let withDb f = liftIO $ runSqlPersistMPool f pool
 
-    NoLoggingT $ runSpock 8080 $ spockT id $ do
+  -- These are shaped like REST endpoints but really, really aren't
+  get ("state" <//> var <//> "process" <//> var) $ \stateId processId -> do
+    state <- withDb $ getProcessState (toSqlKey stateId) (toSqlKey processId)
 
-      -- These are shaped like REST endpoints but really, really aren't
-      get ("state" <//> var <//> "process" <//> var) $ \stateId processId -> do
-        state <- withDb $ getProcessState (toSqlKey stateId) (toSqlKey processId)
+    json (StateForProcess state)
 
-        json (StateForProcess state)
+  post ("state" <//> var <//> "process" <//> var <//> "command") $ \stateId processId -> do
+    (Just methodType) <- param "methodType"
+    (Just method) <- param "method"
+    (Just desc) <- param "desc"
+    (Just note) <- param "note"
+    resultStateId <- param "resultStateId"
+    withDb $ do -- Transaction???
+      commandId <- P.insert $ Command (toSqlKey stateId) methodType method desc (toSqlKey <$> read <$> resultStateId) 
+      cpId <- P.insert $ CommandProcess (toSqlKey processId) commandId note
+      return ()
+    return ()
 
-      post ("state" <//> var <//> "process" <//> var <//> "command") $ \stateId processId -> do
-        (Just methodType) <- param "methodType"
-        (Just method) <- param "method"
-        (Just desc) <- param "desc"
-        (Just note) <- param "note"
-        resultStateId <- param "resultStateId"
-        withDb $ do -- Transaction???
-          commandId <- insert $ Command (toSqlKey stateId) methodType method desc (toSqlKey <$> read <$> resultStateId) 
-          cpId <- insert $ CommandProcess (toSqlKey processId) commandId note
+  post ("command" <//> var) $ \commandIdRaw -> do
+    (Just methodType) <- param "methodType"
+    (Just method) <- param "method"
+    (Just desc) <- param "desc"
+    resultStateId <- param "resultStateId"
+
+    withDb $ P.update (toSqlKey commandIdRaw)
+      [ CommandResultStateId =. toSqlKey <$> read <$> resultStateId
+      , CommandMethodType =. methodType
+      , CommandMethod =. method
+      , CommandDescription =. desc 
+      ]
+
+    return ()
+
+  -- Add a command to a process/edit a command-process
+  post ("command" <//> var <//> "process" <//> var) $ \(commandId :: Int64) (processId :: Int64) -> do
+    (Just note) <- param "note"
+
+    withDb $ do
+      found <- E.select $ from $ \cp -> do
+        where_ (cp ^. CommandProcessCommandId ==. val (toSqlKey commandId) &&. 
+                cp ^. CommandProcessProcessId ==. val (toSqlKey processId))
+        return cp
+
+      case found of
+        [cp] -> do
+          P.update (entityKey cp) [ CommandProcessNotes =. note ]
           return ()
-        return ()
+        [] -> do
+          P.insert $ CommandProcess (toSqlKey processId) (toSqlKey commandId) note
+          return ()
 
-      post ("command" <//> var) $ \commandIdRaw -> do
-        (Just methodType) <- param "methodType"
-        (Just method) <- param "method"
-        (Just desc) <- param "desc"
-        resultStateId <- param "resultStateId"
-         
-        withDb $ update (toSqlKey commandIdRaw)
-          [ CommandResultStateId =. toSqlKey <$> read <$> resultStateId
-          , CommandMethodType =. methodType
-          , CommandMethod =. method
-          , CommandDescription =. desc 
-          ]
-
-        return ()
-
-      -- Add a command to a process/edit a command-process
-      post ("command" <//> var <//> "process" <//> var) $ \(commandId :: Int64) (processId :: Int64) -> do
-        (Just note) <- param "note"
-
-        withDb $ do
-          found <- select $ from $ \cp -> do
-            where_ (cp ^. CommandProcessCommandId ==. val (toSqlKey commandId) &&. 
-                    cp ^. CommandProcessProcessId ==. val (toSqlKey processId))
-            return cp
-
-          case found of
-            [cp] -> do
-              update (entityKey cp) [ CommandProcessNotes =. note ]
-              return ()
-            [] -> do
-              insert $ CommandProcess (toSqlKey processId) (toSqlKey commandId) note
-              return ()
 
 getProcessState :: StateId -> ProcessId -> SqlPersistM (Entity State, [(Entity Command, Maybe (Entity CommandProcess))])
 getProcessState stateId processId = do
-  [state] <- select $ from $ \state -> where_ (state ^. StateId ==. val stateId) >> return state
-  procCommands <- select $ from $ \(c `LeftOuterJoin` cp) -> do
+  [state] <- E.select $ from $ \state -> where_ (state ^. StateId ==. val stateId) >> return state
+  procCommands <- E.select $ from $ \(c `LeftOuterJoin` cp) -> do
     on (just (c ^. CommandId) ==. cp ?. CommandProcessCommandId &&. cp ?. CommandProcessProcessId ==. just (val processId))
     where_ (c ^. CommandStateId ==. val stateId)
     return (c, cp)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,20 @@
+{-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
+module Main (main) where
+
+import Test.Hspec
+import Test.Hspec.Wai
+import Test.Hspec.Wai.JSON
+import Data.Aeson (Value(..), object, (.=))
+
+import Web.Spock.Shared
+
+import Lib (app)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+spec :: Spec
+spec = with (spockAsApp $ app undefined) $ do
+  describe "GET /" $ do
+    it "responds with 404 because broken" $ do
+      get "/" `shouldRespondWith` 404
